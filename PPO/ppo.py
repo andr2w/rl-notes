0680c3ed0fff3e3d0e9cc35f5880f441d223c761
Andrew <20448589@life.hkbu.edu.hk>
2023-08-11 09:23:26 +0800

    a

---
+++ PPO/ppo.py
@@ -0,0 +1,51 @@
+import os 
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions import Categorical
+
+class PPOMemory:
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = [] # policy
+        self.vals = [] # value of critc output
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+
+        self.batch_size = batch_size
+
+    def generate_batches(self):
+        n_states = len(self.states)
+        batch_start = np.arange(0, n_states, self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(indices)
+        batches = [indices[i:i+self.batch_size] for i in batch_start]
+        return np.array(self.states),\
+                np.array(self.actions),\
+                np.array(self.probs),\
+                np.array(self.vals),\
+                np.array(self.rewards),\
+                np.array(self.dones),\
+                batches
+    
+    def store_memory(self, state, action, probs, vals, reward, done):
+        self.states.append(state)
+        self.probs.append(probs)
+        self.vals.append(vals)
+        self.rewards.append(reward)
+        self.dones.append(done)
+
+    def clear_memory(self):
+        self.states = []
+        self.probs = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.vals = []
+    
+class ActorNetwork(nn.Module):
+    def __init__(self, n_actions, input_dims, lr, fc1_dims=256, fc2_dims=256):
+        super(ActorNetwork, self).__init__()
+
